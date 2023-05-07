315c5cc517c19e4f1c2723b5c5f0e76de70e5bd6
Ali <56807795+xAlidev@users.noreply.github.com>
2023-05-07 04:26:28 +0300

    just update Home page

--- pages/index.js
+++ pages/index.js
@@ -5,12 +5,23 @@ import Layout from '../layouts/layout';
 const Home = () => {
   return (
     <Layout>
-      <h1 className="text-4xl font-semibold">Home Page</h1>
-      <Link href="/dashboard">
-        <span className="text-white bg-indigo-500 rounded-lg py-2 px-4 hover:bg-indigo-600 cursor-pointer">
-          Go to Dashboard
-        </span>
-      </Link>
+      <div className="container mx-auto p-8">
+        <div className="bg-white p-6 rounded-xl shadow-lg max-w-md mx-auto">
+          <h1 className="text-4xl font-semibold mb-6 text-center text-indigo-600">
+            Home Page
+          </h1>
+          <p className="text-gray-700 mb-4">
+            This is a simple example of a Next.js application with a dashboard.
+          </p>
+          <div className="flex justify-center">
+            <Link href="/dashboard">
+              <span className="inline-block text-white bg-indigo-500 rounded-lg py-2 px-4 hover:bg-indigo-600 cursor-pointer">
+                Go to Dashboard
+              </span>
+            </Link>
+          </div>
+        </div>
+      </div>
     </Layout>
   );
 };
