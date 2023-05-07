6788a643757d7348fda3a2f91618c014cf71896e
Ali <56807795+xAlidev@users.noreply.github.com>
2023-05-07 04:29:09 +0300

    just update home page

--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,51 @@
 // pages/index.js
 import Link from 'next/link';
 import Layout from '../layouts/layout';
+import { motion } from 'framer-motion';
 
 const Home = () => {
   return (
     <Layout>
-      <div className="container mx-auto p-8">
-        <div className="bg-white p-6 rounded-xl shadow-lg max-w-md mx-auto">
-          <h1 className="text-4xl font-semibold mb-6 text-center text-indigo-600">
-            Home Page
-          </h1>
-          <p className="text-gray-700 mb-4">
-            This is a simple example of a Next.js application with a dashboard.
-          </p>
-          <div className="flex justify-center">
-            <Link href="/dashboard">
-              <span className="inline-block text-white bg-indigo-500 rounded-lg py-2 px-4 hover:bg-indigo-600 cursor-pointer">
-                Go to Dashboard
-              </span>
-            </Link>
+      <div className="container mx-auto px-4">
+        <div className="h-screen flex flex-col justify-center items-center">
+          <div className="text-center">
+            <motion.h1
+              className="text-5xl font-semibold mb-6 text-indigo-600"
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ duration: 1 }}
+            >
+              Home Page
+            </motion.h1>
+            <motion.p
+              className="text-gray-700 mb-8"
+              initial={{ opacity: 0, y: -10 }}
+              animate={{ opacity: 1, y: 0 }}
+              transition={{ duration: 1, delay: 0.5 }}
+            >
+              This is a simple example of a Next.js application with a dashboard.
+            </motion.p>
+            <motion.div
+              className="flex justify-center"
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ duration: 1, delay: 1 }}
+            >
+              <Link href="/dashboard">
+                <span className="inline-block text-white bg-indigo-500 rounded-lg py-2 px-4 hover:bg-indigo-600 cursor-pointer">
+                  Go to Dashboard
+                </span>
+              </Link>
+            </motion.div>
           </div>
         </div>
+        <footer className="py-6 bg-indigo-600">
+          <div className="container mx-auto text-center">
+            <p className="text-white">
+              &copy; {new Date().getFullYear()} - My Application
+            </p>
+          </div>
+        </footer>
       </div>
     </Layout>
   );
